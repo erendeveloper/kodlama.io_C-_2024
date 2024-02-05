2e48f72db4e2a13d3a709668efb5868a7e00b939
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2024-02-06 01:54:14 +0300

    day 1 hw 2
    day 1 hw 2

---
+++ Day 1/hw 2/MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new();
+person1.FirstName = "Super";
+person1.LastName = "Maryo";
+person1.DateOfBirthYear = 1989;
+person1.NationalIdentity = 12345678910;
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
